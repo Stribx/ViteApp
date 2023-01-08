8a4917c4a7bd4326a17cab6033bba3e6b3ef82b8
Stribx <Stribx@users.noreply.github.com>
2023-01-08 21:20:08 +0100

    fix settings icon

--- src/components/button/Darkmode.jsx
+++ src/components/button/Darkmode.jsx
@@ -1,37 +1,59 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+
+const variants = {
+  enter: (direction) => {
+    return {
+      x: direction > 0 ? 100 : -100,
+      opacity: 0
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: (direction) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 100 : -100,
+      opacity: 0
+    };
+  }
+};
 
 const DarkMode = memo(({ isDarkMode, setIsDarkMode }) => {
+  const [[icon, direction], setIcon] = useState([0, 0]);
+  const paginate = (newDirection) => {
+    setIcon([icon + newDirection, newDirection]);
+  }
+
   function toggleDarkMode() {
     setIsDarkMode(!isDarkMode);
     document.body.classList.toggle("lightMode", !isDarkMode);
+    paginate(1);
   }
 
   return (
     <div className="mode">
-      <AnimatePresence>
+      <AnimatePresence mode="wait" initial={false} custom={direction}>
         <motion.button
-          initial={{ x: -100 }}
-          animate={{ x: 0 }}
-          exit={{ x: 100 }}
+          key={icon}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 }
+          }}
           whileTap={{ scale: 0.8 }}
-          className={`dark-mode-button ${isDarkMode ? "hidden" : ""}`}
+          className={`dark-mode-button`}
           onClick={toggleDarkMode}
         >
-          <IoSunnyOutline />
-        </motion.button>
-      </AnimatePresence>
-      <AnimatePresence>
-        <motion.button
-          initial={{ x: -100 }}
-          animate={{ x: 0 }}
-          exit={{ x: 100 }}
-          whileTap={{ scale: 0.8 }}
-          className={`dark-mode-button ${isDarkMode ? "" : "hidden"}`}
-          onClick={toggleDarkMode}
-        >
-          <IoMoonOutline />
+          {isDarkMode ? <IoSunnyOutline /> : <IoMoonOutline />}
         </motion.button>
       </AnimatePresence>
     </div>
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,17 +1,14 @@
 import "./Settings.css";
 import DarkMode from "../../components/button/Darkmode";
-import { motion, AnimatePresence } from "framer-motion";
 
 export default function Settings(props) {
   return (
-    <motion.div className="Settings">
+    <div className="Settings">
       <h1>Settings</h1>
-      <AnimatePresence mode="wait">
         <DarkMode
           isDarkMode={props.isDarkMode}
           setIsDarkMode={props.setIsDarkMode}
         />
-      </AnimatePresence>
-    </motion.div>
+    </div>
   );
 }
